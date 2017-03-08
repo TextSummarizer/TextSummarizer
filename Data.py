feae0c5a48e20f5da33bf97bf69d1bbf3bb7db40
gianni.mastroscianni <giannisoad12@gmail.com>
2017-03-08 17:53:39 +0100

    tab fixed

--- Data.py
+++ Data.py
@@ -3,71 +3,71 @@ from nltk.tokenize import RegexpTokenizer
 
 
 class Data:
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def stemming(self):
-    pass
+    def stemming(self):
+        pass
 
-  def remove_punctuation(self, data):
-    to_return = []
-    tokenizer = RegexpTokenizer(r'\w+')
-    for sentence in data:
-      temp = ""
-      tokenized = tokenizer.tokenize(sentence)
-      for word in tokenized:
-        temp += word
-        temp += " "
-      to_return.append(temp)
-    return to_return
+    def remove_punctuation(self, data):
+        to_return = []
+        tokenizer = RegexpTokenizer(r'\w+')
+        for sentence in data:
+            temp = ""
+            tokenized = tokenizer.tokenize(sentence)
+            for word in tokenized:
+                temp += word
+                temp += " "
+            to_return.append(temp)
+        return to_return
 
-  def delete_stopwords(self, data):
-    to_return = []
-    stop = set(stopwords.words('english'))
-    for sentence in data:
-      stopped = ""
-      sentence = sentence.split(" ")
-      temp = [i for i in sentence if i not in stop]
-      for word in temp:
-        stopped += word
-        stopped += " "
-      to_return.append(stopped)
-    return to_return
+    def delete_stopwords(self, data):
+        to_return = []
+        stop = set(stopwords.words('english'))
+        for sentence in data:
+            stopped = ""
+            sentence = sentence.split(" ")
+            temp = [i for i in sentence if i not in stop]
+            for word in temp:
+                stopped += word
+                stopped += " "
+            to_return.append(stopped)
+        return to_return
 
-  def get_data(self, data):
-    to_return = []
-    f = open(data, "r")
-    i = 0
-    while i < len(
-      data):  # scandisce fino alla fine dei giorni. per risolvere non ho salvato nell'array da ritornare le frasi(stringhe) vuote
-      line = f.readline()
-      sentences = line.lower().split(". ")
-      for sentence in sentences:
-        if sentence != "\n" and sentence != '':
-          if sentence.startswith(" "):
-            sentence.replace(" ", "")
-          to_return.append(sentence)
-      i += 1
-    f.close()
-    return to_return
+    def get_data(self, data):
+        to_return = []
+        f = open(data, "r")
+        i = 0
+        while i < len(
+                data):  # scandisce fino alla fine dei giorni. per risolvere non ho salvato nell'array da ritornare le frasi(stringhe) vuote
+            line = f.readline()
+            sentences = line.lower().split(". ")
+            for sentence in sentences:
+                if sentence != "\n" and sentence != '':
+                    if sentence.startswith(" "):
+                        sentence.replace(" ", "")
+                    to_return.append(sentence)
+            i += 1
+        f.close()
+        return to_return
 
-  def print_file(self, data, out):
-    f = open(out, "w")
-    for string in data:
-      f.write(string)
-    f.close()
+    def print_file(self, data, out):
+        f = open(out, "w")
+        for string in data:
+            f.write(string)
+        f.close()
 
-  def print_data(self, data):
-    for row in data:
-      print row
+    def print_data(self, data):
+        for row in data:
+            print row
 
-  def write_on(self, data, out):
-    out = open(out, "w")
-    for row in data:
-      line = ""
-      for i in row:
-        line += i
-      out.write(line)
+    def write_on(self, data, out):
+        out = open(out, "w")
+        for row in data:
+            line = ""
+            for i in row:
+                line += i
+            out.write(line)
 
 
 d = Data()
